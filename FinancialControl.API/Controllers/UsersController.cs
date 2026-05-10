f71057707d6fbc75a8642254c47ee9392c90746b
Ney Leal <oliveira.ney7@gmail.com>
2026-05-10 11:23:27 -0300

    feat: implement user creation endpoint

---
+++ FinancialControl.API/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using FinancialControl.Application.DTOs;
+using FinancialControl.Application.UseCases;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinancialControl.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController: ControllerBase
+    {
+        private readonly CreateUserUseCase  _createUserUseCase;
+
+        public UsersController(CreateUserUseCase createUserUseCase)
+        {
+            _createUserUseCase = createUserUseCase;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserDto dto)
+        {
+            var result = await _createUserUseCase.Execute(dto);
+            return Ok(result);
+        }
+    }
+}
