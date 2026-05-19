fa3813820ea461e9b8cd9084874ad2bcbb87560f
Ney Leal <oliveira.ney7@gmail.com>
2026-05-19 16:08:19 -0300

    feat: implement financial summary endpoint

--- FinancialControl.API/Controllers/TransactionsController.cs
+++ FinancialControl.API/Controllers/TransactionsController.cs
@@ -1,8 +1,10 @@
 ﻿using FinancialControl.Application.DTOs;
 using FinancialControl.Domain.Entities;
+using FinancialControl.Domain.Enums;
 using FinancialControl.Infrastructure.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
 using System.Security.Claims;
 
 namespace FinancialControl.API.Controllers
@@ -24,11 +26,12 @@ namespace FinancialControl.API.Controllers
         {
             var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
 
-            return Ok(new
-            {
-                message = "Protected route accessed successfully",
-                userId
-            });
+            var transactions = _context.Transactions
+                .Where(t => t.UserId == Guid.Parse(userId))
+                .OrderByDescending(t => t.Date)
+                .ToList();
+
+            return Ok(transactions);
         }
         [HttpPost]
         public async Task<IActionResult> Create(CreateTransactionDto dto)
@@ -45,6 +48,34 @@ namespace FinancialControl.API.Controllers
             _context.Transactions.Add(transaction);
             await _context.SaveChangesAsync();
             return Ok(transaction);
+        }
+        [HttpGet("summary")]
+        public IActionResult GetSummary()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            var transactions = _context.Transactions
+                .Where(t => t.UserId == Guid.Parse(userId!))
+                .OrderByDescending(t => t.Date)
+                .ToList();
+
+            var income = transactions
+                .Where(t => t.Type == TransactionType.Income)
+                .Sum(t => t.Amount);
+
+            var expenses = transactions
+                .Where(t => t.Type == TransactionType.Expense)
+                .Sum(t => t.Amount);
+
+            var summary = new TransactionSummaryDto
+            {
+                Income = income,
+                Expenses = expenses,
+                Balance = income - expenses
+
+            };
+
+            return Ok(summary);
         }
     }
 }
---
+++ FinancialControl.Application/DTOs/TransactionSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FinancialControl.Application.DTOs
+{
+    public class TransactionSummaryDto
+    {
+        public decimal Income { get; set; }
+        public decimal Expenses { get; set; }
+        public decimal Balance { get; set; }
+    }
+}
